a9bbca8c652b7a35e2f7efc910fa6b02793310f0
xhs <2018151104@email.szu.edu.cn>
2020-11-16 12:15:58 +0800

    add a project

---
+++ t1.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"Git is a version control system."<<endl;
+	cout<<"Git is free software."<<endl;
+ } 
---
+++ t2.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"I`m going to study Git now!"<<endl;
+	cout<<"Wish me a good luck!"<<endl;
+ } 
