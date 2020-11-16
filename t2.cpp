4193c4582d4416d88d5119e8d2af218ab0ff966d
xhs <2018151104@email.szu.edu.cn>
2020-11-16 12:34:30 +0800

    last change

--- t2.cpp
+++ t2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
 	cout<<"Git is a distributed version control system."<<endl;
-	cout<<"Git is free software."<<endl;
+	cout<<"Git is free software distributed under the GPL."<<endl;
 	cout<<"I`m going to study Git now!"<<endl;
 	cout<<"Wish me a good luck!"<<endl;
  } 
