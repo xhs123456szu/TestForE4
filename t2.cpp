cb6992cdfe5660219046e1096c476f1784ef76ca
xhs <2018151104@email.szu.edu.cn>
2020-11-16 12:32:23 +0800

    second change

--- t2.cpp
+++ t2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-	cout<<"Git is a version control system."<<endl;
+	cout<<"Git is a distributed version control system."<<endl;
 	cout<<"I`m going to study Git now!"<<endl;
 	cout<<"Wish me a good luck!"<<endl;
  } 
