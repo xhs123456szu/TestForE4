58dd14b1051d779e819d2fc1320ba2f756b366ff
xhs <2018151104@email.szu.edu.cn>
2020-11-16 12:33:42 +0800

    third change

--- t2.cpp
+++ t2.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
 	cout<<"Git is a distributed version control system."<<endl;
+	cout<<"Git is free software."<<endl;
 	cout<<"I`m going to study Git now!"<<endl;
 	cout<<"Wish me a good luck!"<<endl;
  } 
